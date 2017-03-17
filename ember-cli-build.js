da0b7991fdd8e4436c969758a9670125868c053b
Gabriel Cousin <csngab@gmail.com>
2017-03-17 18:18:33 +0100

    Style | Use SASS and Tapestry

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,8 +2,8 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
+  const app = new EmberApp(defaults, {
+    sassOptions: {}
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -18,6 +18,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('bower_components/tapestry/dist/fonts/tapestry-icons.woff2', {
+    destDir: 'assets'
+  });
 
   return app.toTree();
 };
