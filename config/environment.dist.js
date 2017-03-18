bebcc38e912fd5065e8dfbaea4c8adefece304dd
Gabriel Cousin <csngab@gmail.com>
2017-03-18 17:31:28 +0100

    Config | Add API key and total rounds

--- config/environment.dist.js
+++ config/environment.dist.js
@@ -20,6 +20,8 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+      MOVIEDB_APIKEY: 'MOVIEDB_APIKEY',
+      QUESTION_PER_ROUND: 10
     }
   };
 
