1e6cfa428c5b8fee6332f8aebc4b359a7ff516dd
Gabriel Cousin <csngab@gmail.com>
2017-03-18 17:32:53 +0100

    Quiz | Add first implementation

--- app/controllers/quiz.js
+++ app/controllers/quiz.js
@@ -1,7 +1,60 @@
 import Ember from 'ember';
+import config from 'moviequiz/config/environment';
 
 export default Ember.Controller.extend({
 
-  isFetching: false
+  totalQuestions: config.APP.QUESTION_PER_ROUND,
+
+  movieImageStyle: Ember.computed('currentQuestion', function() {
+    let path = this.get('currentQuestion.movieImage');
+    return Ember.String.htmlSafe(`background-image: url("https://image.tmdb.org/t/p/w500${path}")`);
+  }),
+
+  actorImageStyle: Ember.computed('currentQuestion', function() {
+    let path = this.get('currentQuestion.actorImage');
+    return Ember.String.htmlSafe(`background-image: url("https://image.tmdb.org/t/p/w500${path}")`);
+  }),
+
+  setNextQuestion () {
+    // @TODO improve algo with more matching questions
+    let movies = this.get('movies');
+    let actors = this.get('actors');
+
+    let randomMovieIndex = Math.floor(Math.random() * movies.length);
+    let randomActorIndex = Math.floor(Math.random() * actors.length);
+
+    let randomMovie = movies[randomMovieIndex];
+    let randomActor = actors[randomActorIndex];
+
+    let currentQuestion = {
+      movieTitle: randomMovie.get('title'),
+      movieImage: randomMovie.get('image'),
+      actorName: randomActor.get('name'),
+      actorImage: randomActor.get('image'),
+      // @TODO replace parseInt hack
+      match: randomActor.get('movies').indexOf(parseInt(randomMovie.get('id'))) !== -1
+    }
+
+    actors.splice(randomActorIndex, 1)
+
+    this.set('actors', actors);
+    this.set('currentQuestion', currentQuestion);
+    this.set('currentIndex', this.get('currentIndex') + 1);
+  },
+
+  actions: {
+    checkAnswer(answer) {
+      let index = this.get('currentIndex');
+      let totalQuestions = this.get('totalQuestions');
+      let gameOver = this.get('currentQuestion.match') !== answer;
+
+      if (gameOver || index === totalQuestions) {
+        this.transitionToRoute('leaderboard');
+        return;
+      }
+
+      this.setNextQuestion()
+    }
+  }
 
 });
