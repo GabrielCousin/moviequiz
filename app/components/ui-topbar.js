5ba72f2e0b9c7fc5c18f1734edaaf1fa2e8aff68
Gabriel Cousin <csngab@gmail.com>
2017-03-19 12:09:26 +0100

    Topbar | Add username when logged-in

--- app/components/ui-topbar.js
+++ app/components/ui-topbar.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  storage: Ember.inject.service(),
+
   tagName: 'nav',
-  classNames: ['Topbar']
+  classNames: ['Topbar'],
+
+  username: Ember.computed('storage.username', function() {
+    return this.get('storage.username');
+  })
 });
