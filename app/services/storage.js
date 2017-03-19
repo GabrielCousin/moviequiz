9fbb7582c9320d29396a14c843547e55c8884310
Gabriel Cousin <csngab@gmail.com>
2017-03-19 15:09:08 +0100

    Leaderboard | Save highscores into localStorage

--- app/services/storage.js
+++ app/services/storage.js
@@ -7,16 +7,20 @@ export default Ember.Service.extend({
 
   init() {
     const username = localStorage.getItem('moviequiz_user');
+    const highscores = localStorage.getItem('moviequiz_scores');
 
     if (username)
       this.signUserIn(username)
+
+    if (highscores)
+      this.recoverLeaderboard(highscores)
   },
 
-  getEntry(key) {
+  getItem(key) {
     return localStorage.getItem(key);
   },
 
-  setEntry(key, value) {
+  setItem(key, value) {
     localStorage.setItem(key, value);
   },
 
@@ -28,6 +32,40 @@ export default Ember.Service.extend({
     localStorage.clear();
   },
 
+  recoverLeaderboard(highscores) {
+    const store = this.get('store');
+    let _highscores = JSON.parse(highscores) || [];
+
+    if (!_highscores.length)
+      return;
+
+    _highscores.forEach(function (score) {
+      store.createRecord('score', score);
+    })
+  },
+
+  addHighscore(name, score) {
+    let highscores = JSON.parse(this.getItem('moviequiz_scores')) || [];
+
+    highscores.push({
+      name: name,
+      score: score
+    });
+
+    this.setItem('moviequiz_scores', JSON.stringify(highscores));
+  },
+
+  resetHighscores() {
+    const store = this.get('store');
+    const scores = store.peekAll('score').toArray();
+
+    scores.forEach(function (score) {
+      score.deleteRecord();
+    })
+
+    this.removeItem('moviequiz_scores');
+  },
+
   signUserIn(username) {
     const store = this.get('store');
     this.set('username', username)
@@ -36,7 +74,7 @@ export default Ember.Service.extend({
       name: username
     });
 
-    this.setEntry('moviequiz_user', username)
+    this.setItem('moviequiz_user', username)
   },
 
 });
