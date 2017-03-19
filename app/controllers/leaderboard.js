d9d64a1e13939458cfd89a713c622a3c8111d4bc
Gabriel Cousin <csngab@gmail.com>
2017-03-19 13:28:16 +0100

    Leaderboard | Add reset and play again link

--- app/controllers/leaderboard.js
+++ app/controllers/leaderboard.js
@@ -1,8 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  // @TODO sort http://emberjs.com/api/classes/Ember.computed.html#method_sort
+  scores: Ember.computed.alias('model'),
 
-  scores: Ember.computed('model', function () {
-    return this.get('model').sortBy('score').reverse();
-  })
+  resetLeaderboard() {
+    const store = this.get('store');
+    const scores = store.peekAll('score').toArray();
+
+    scores.forEach(function (score) {
+      score.deleteRecord();
+    })
+  },
+
+  actions: {
+    onResetLeaderboard() {
+      this.resetLeaderboard();
+    }
+  }
 });
