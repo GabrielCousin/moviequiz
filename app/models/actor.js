86f7bcdd218ab682df884f03bd72caac85f4d608
Gabriel Cousin <csngab@gmail.com>
2017-03-27 07:24:39 +0200

    Models | Add hasMany relation

--- app/models/actor.js
+++ app/models/actor.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  image: DS.attr('string'),
-  movies: DS.attr()
+  profile_path: DS.attr('string'),
+  movies: DS.hasMany('movie')
 });
--- app/models/movie.js
+++ app/models/movie.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  image: DS.attr('string'),
+  actors: DS.hasMany('actor'),
+  poster_path: DS.attr('string'),
+  media_type: DS.attr('string'),
   title: DS.attr('string')
 })
