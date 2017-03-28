829e2ebf010179393026ae3289a6e99110f9639f
Gabriel Cousin <csngab@gmail.com>
2017-03-28 23:14:29 +0200

    Leaderboard | Add save to server feature

--- app/components/quiz-leaderboard/component.js
+++ app/components/quiz-leaderboard/component.js
@@ -3,12 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['Leaderboard'],
   scores: Ember.computed.sort('model', 'scoreSorting'),
+  saveLabel: "Save to server",
 
   scoreSorting: ['score:desc'],
 
   actions: {
-    onResetLeaderboard() {
-      this.get('onResetLeaderboard')();
+    onSaveToServer() {
+      this.set('saveLabel', 'Saving');
+      this.get('onSaveToServer')().then(() => {
+        this.set('saveLabel', 'Saved!');
+      }, () => {
+        this.set('saveLabel', 'Failed :(');
+      })
     }
   }
 });
--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -10,6 +10,31 @@ export default Ember.Route.extend({
     onResetLeaderboard() {
       const storage = this.get('storage');
       storage.resetHighscores();
+    },
+
+    onSaveToServer() {
+      const store = this.get('store');
+      const highscores = store.peekAll('score').toArray();
+
+      let data = highscores.map(function(highscore) {
+        return {
+          name: highscore.get('name'),
+          score: highscore.get('score')
+        }
+      })
+
+      return new Ember.RSVP.Promise((resolve, reject) => {
+        Ember.$.ajax({
+           url: 'http://wrongurl.net',
+           type: 'POST',
+           data: JSON.stringify(data)
+        }).done(() => {
+          resolve()
+        }).fail(() => {
+          reject()
+        });
+      });
     }
+
   }
 });
