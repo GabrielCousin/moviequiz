91f824eefac13010329c5ff6d2fd42b6bef8b3e7
Gabriel Cousin <csngab@gmail.com>
2017-03-28 21:21:36 +0200

    Leaderboard | Use quiz-leaderboard component

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  storage: Ember.inject.service(),
 
   username: '',
   isInvalid: false,
--- app/initializers/storage.js
+++ app/initializers/storage.js
@@ -1,6 +1,5 @@
 export function initialize (application) {
   application.inject('route', 'storage', 'service:storage');
-  application.inject('controller', 'storage', 'service:storage');
 }
 
 export default {
