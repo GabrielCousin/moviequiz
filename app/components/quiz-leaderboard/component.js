182f360203bfb59a3e6cd8f78668ad7c8b475975
Gabriel Cousin <csngab@gmail.com>
2017-03-28 21:21:36 +0200

    Leaderboard | Use quiz-leaderboard component

--- app/components/quiz-leaderboard/component.js
+++ app/components/quiz-leaderboard/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  scores: Ember.computed.sort('model', 'scoreSorting'),
+
+  scoreSorting: ['score:desc'],
+
+  actions: {
+    onResetLeaderboard() {
+      this.get('onResetLeaderboard')();
+    }
+  }
 });
--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -4,5 +4,12 @@ export default Ember.Route.extend({
   model() {
     const store = this.get('store');
     return store.peekAll('score');
+  },
+
+  actions: {
+    onResetLeaderboard() {
+      const storage = this.get('storage');
+      storage.resetHighscores();
+    }
   }
 });
