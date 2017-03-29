f2672e6a1c834eee96a183bdc81381d5dd6c97da
Gabriel Cousin <csngab@gmail.com>
2017-03-29 22:23:48 +0200

    Tests | Check inlineBackground computed value

--- tests/unit/helpers/inline-background-test.js
+++ tests/unit/helpers/inline-background-test.js
@@ -1,12 +1,11 @@
-
+import Ember from 'ember';
 import { inlineBackground } from 'moviequiz/helpers/inline-background';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | inline background');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = inlineBackground([42]);
-  assert.ok(result);
+test('should inline background style from img path', function (assert) {
+  let result = inlineBackground(['/img.jpg']);
+  let expected = Ember.String.htmlSafe('background-image: url("https://image.tmdb.org/t/p/w500/img.jpg")');
+  assert.equal(result.text, expected.text);
 });
-
